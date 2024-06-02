7c477f199935f0151355ac55a29a9c0fa5a39f7d
ihsan-afridi <ihsanafridi576284@gmail.com>
2024-06-02 06:43:39 -0700

    projects

---
+++ java.js
@@ -0,0 +1,14 @@
+// var mony = +prompt ("Enter mony ")
+// if (mony >= 55) {
+//     alert("you can go!")
+// }
+// if (mony < 55) {
+//     alert("you are not allowed!")
+// }
+
+
+function name(num1 , num2) {
+    return num1 + num2
+}
+const myname = name(22,33)
+console.log(myname);
